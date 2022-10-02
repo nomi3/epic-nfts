cdcdf950a81d7b2086bdce9b5a06925321f4b052
nomi3 <thisisnomi3@gmail.com>
2022-10-02 21:56:59 +0900

    引数に名前の追加

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,7 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
   // makeAnEpicNFT 関数を呼び出す。NFT が Mint される。
-  let txn = await nftContract.makeAnEpicNFT({
+  let txn = await nftContract.makeAnEpicNFT("Super Artist",{
     value: hre.ethers.utils.parseEther("0.01"),
   });
   // Minting が仮想マイナーにより、承認されるのを待つ。
