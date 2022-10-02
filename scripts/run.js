fcca1da19baa38f88ffe9a8a808233e04742a013
nomi3 <thisisnomi3@gmail.com>
2022-10-02 17:20:31 +0900

    価格の設定

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,9 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
   // makeAnEpicNFT 関数を呼び出す。NFT が Mint される。
-  let txn = await nftContract.makeAnEpicNFT();
+  let txn = await nftContract.makeAnEpicNFT({
+    value: hre.ethers.utils.parseEther("0.01"),
+  });
   // Minting が仮想マイナーにより、承認されるのを待つ。
   await txn.wait();
 };
